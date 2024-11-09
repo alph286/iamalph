a274658517ccf87f4aa0da656c5f31129de859de
alph286 <alph286@gmail.com>
2024-11-09 21:08:36 +0100

    fixed dot on mobile

--- components/bg/dot.tsx
+++ components/bg/dot.tsx
@@ -2,12 +2,10 @@ import { motion } from 'framer-motion';
 
 interface DotProps {
   className?: string;
-  
 }
 
 export default function Dot({ className }: DotProps) {
   return (
-    
     <motion.div
       className={className}
       initial={{ opacity: 0, scale: 0.5 }}
@@ -19,23 +17,35 @@ export default function Dot({ className }: DotProps) {
           type: "spring",
           damping: 15,
           stiffness: 150,
-          restDelta: 0.001
-        }
-        
+          restDelta: 0.001,
+        },
       }}
       whileHover={{
         scale: 1.2,
-       
-        transition: { duration: 0.3, 
-            ease: [0, 0.71, 0.2, 1.01],
-        scale: {
-          type: "spring",
-          damping: 15,
-          stiffness: 150,
-          restDelta: 0.001
-        } },
+        transition: {
+          duration: 0.3,
+          ease: [0, 0.71, 0.2, 1.01],
+          scale: {
+            type: "spring",
+            damping: 15,
+            stiffness: 150,
+            restDelta: 0.001,
+          },
+        },
+      }}
+      whileTap={{
+        scale: 1.2,
+        transition: {
+          duration: 0.3,
+          ease: [0, 0.71, 0.2, 1.01],
+          scale: {
+            type: "spring",
+            damping: 15,
+            stiffness: 150,
+            restDelta: 0.001,
+          },
+        },
       }}
     />
-   
   );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
 return (
     <div className="w-full h-screen p-4 bg-green-300  overflow-hidden relative flex items-center justify-center ">
     
-        <Dot className="w-screen aspect-square absolute left-0 top-0 rounded-full bg-slate-700 "/>
+        <Dot className="h-screen md:h-auto md:w-screen aspect-square absolute left-0 top-0 rounded-full bg-slate-700 "/>
     
         {dots.map((_, index) => {
         const randomLeft = Math.floor(Math.random() * 90); // Genera una posizione random tra 0 e 90vw
